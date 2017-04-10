5bf2588b48b7f912b1225f1c844a29bd2108be38
Ignacio Losiggio <iglosiggio@gmail.com>
2017-04-10 20:18:54 -0300

    Fallando en integrarlo a semantic

--- app/components/repo-search.js
+++ app/components/repo-search.js
@@ -2,18 +2,56 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   api: Ember.inject.service(),
+  fullResults: [],
+  limit: undefined,
+  page: 0,
 
-  debounceSearch() {
-    Ember.run.debounce(this, this.updateSearch, 70, true);
+  debounceSearch: Ember.observer('search', function() {
+    Ember.run.debounce(this, this.updateSearch, 150);
+    console.log(this.get('search'));
+  }),
+
+  results: Ember.computed('fullResults', 'page', 'limit', function() {
+    const limit = this.get('limit');
+    const page = this.get('page');
+    return this.get('fullResults').slice(limit * page, limit * (page + 1));
+  }),
+
+  disableNext: Ember.computed('fullResults.length', 'page', 'limit', function() {
+    const limit = this.get('limit');
+    const page = this.get('page');
+    const results = this.get('fullResults.length')
+    return results <= limit * (page + 1) || results === 0;
+  }),
+
+  disablePrevious: Ember.computed('page', function() {
+    const page = this.get('page');
+    return page === 0;
+  }),
+
+  next() {
+    const page = this.get('page');
+    this.set('page', page + 1);
+  },
+
+  previous() {
+    const page = this.get('page');
+    this.set('page', page - 1);
   },
 
   updateSearch() {
+    if(this.get('search') === '') {
+      this.set('fullResults', []);
+      return;
+    }
     // This is wrong, this has a race condition bc the callback is not being debounced
     this.get('api').getAllPackages().then((packages) => {
       const search = this.get('search');
-      const regexp = RegExp(`.*${search}.*`, 'g');
+      const limit = this.get('limit');
+      const regexp = new RegExp(`.*${search}.*`, 'g');
       const results = packages.match(regexp);
-      this.set('results', results);
+      this.set('fullResults', results || []);
+      this.set('page', 0);
     });
   }
 });
