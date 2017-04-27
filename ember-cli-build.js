7407b7bdf95e5a403d201ea2068add4cf9167313
Ignacio Losiggio <iglosiggio@gmail.com>
2017-04-27 15:09:42 -0300

    Made EMBER_PREPEND work

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    prepend: process.env.EMBER_PREPEND
+    fingerprint: {
+      prepend: process.env.EMBER_PREPEND
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
